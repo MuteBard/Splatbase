8666ecc92ec9dff9cb5bc26fb894cf6858dcb44f
Carl <carl@Carlas-MacBook-Pro.local>
2017-05-28 14:01:48 -0400

    dadtabase weaponmember pairing near complete

--- dataimport/import.js
+++ dataimport/import.js
@@ -7,88 +7,65 @@ const dbConfig = require('./secret/db-config');
 const db = pgp(dbConfig);
 
 //add members
-fs.createReadStream('splatdata.csv', {encoding: "utf8"})
-  .pipe(csv()) //what does this do?
-  .on('data', function (data) {
-      data.STATUS = (data.STATUS === "In a Team") ? false:true;
-      data.LFT = (data.LFT === "") ? false:true;
-      data.NOT = (data.NOT === "") ? null:data.NOT;
-      db.none(`
-          insert into member values
+// fs.createReadStream('splatdata.csv', {encoding: "utf8"})
+//   .pipe(csv()) //what does this do?
+//   .on('data', function (data) {
+//       data.STATUS = (data.STATUS === "In a Team") ? false:true;
+//       data.LFT = (data.LFT === "") ? false:true;
+//       data.NOT = (data.NOT === "") ? null:data.NOT;
+//       db.none(`
+//           insert into member values
+//             (
+//              DEFAULT,
+//              $(USERNAME),
+//              $(NNID),
+//              null,
+//              null,
+//              $(RANK),
+//              $(STATUS),
+//              $(LFT),
+//              $(NOT),
+//              null,
+//              null
+//             )`, data)
+// });
 
-            (
-             DEFAULT,
-             $(USERNAME),
-             $(NNID),
-             null,
-             null,
-             $(RANK),
-             $(STATUS),
-             $(LFT),
-             $(NOT),
-             null,
-             null
-            )`,
-             data)
-      .catch(error => {
-         console.log(error)
-     });
-});
-fs.createReadStream('splatdata.csv', {encoding: "utf8"})
-  .pipe(csv()) //what does this do?
-  .on('data', function (data) {
-      data.STATUS = (data.STATUS === "In a Team") ? false:true;
-      data.LFT = (data.LFT === "") ? false:true;
-      data.NOT = (data.NOT === "") ? null:data.NOT;
-      db.none(`
-          insert into member values
-            (
-             DEFAULT,
-             $(USERNAME),
-             $(NNID),
-             null,
-             null,
-             $(RANK),
-             $(STATUS),
-             $(LFT),
-             $(NOT),
-             null,
-             null
-            )`,
-             data)
-      .catch(error => {
-         console.log(error)
-     });
-});
+
+// fs.createReadStream('splatdata.csv', {encoding: "utf8"})
+//   .pipe(csv()) //what does this do?
+//   .on('data', function (data) {
+//       data.NOT = (data.NOT === "") ? null:data.NOT;
+//       data.LOGO = (data.LOGO === "") ? null:data.LOGO;
+//       data.CAPTIAN = (data.CAPTIAN === "") ? null:data.CAPTIAN
+//       db.none(`
+//           insert into teams values
+//           (
+//            DEFAULT,
+//            $(NOT),
+//            $(LOGO),
+//            $(CAPTAIN),
+//            null,
+//            null,
+//            null,
+//            null
+//           )`, data)
+// });
 
 fs.createReadStream('splatdata.csv', {encoding: "utf8"})
   .pipe(csv()) //what does this do?
   .on('data', function (data) {
-      data.NOT = (data.NOT === "") ? null:data.NOT;
-      data.LOGO = (data.LOGO === "") ? null:data.LOGO;
+      let list3 = data.LIST3.split()
+      console.log(list3)
       db.none(`
-          insert into team values
+          insert into teams values
           (
-           Default,
-           $(NOT)
-           $(LOGO)
-           $(CAPTAIN)
+           DEFAULT,
+           $(USERNAME),
+           $(LOGO),
+           $(CAPTAIN),
            null,
            null,
            null,
            null
-          `)
+          )`, data)
 });
-
-// CREATE TABLE teams(
-//   id serial primary key,
-//   teamname varchar,
-//   avatar varchar,
-//   captain varchar,
-//   division varchar,
-//   birthday varchar,
-//   aboutme varchar,
-//   views integer,
-//   lfm boolean,
-//   endorse boolean
-// );
